3d275baa6d80d6e626f9ab03a450a26dd5442d59
Kaue <kauemroberto@gmail.com>
2024-04-03 11:20:47 -0300

    Troca de projeto

---
+++ JogoKaue/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace JogoKaue;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ JogoKaue/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace JogoKaue;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
